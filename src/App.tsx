09f699b6a2848477c66e47b963b152554d039b89
BipanKishore <bipankishore@gmail.com>
2023-09-27 19:21:20 +0530

    nest-panes-demo

--- src/App.tsx
+++ src/App.tsx
@@ -6,35 +6,63 @@ import {
 import { CustomResizerFirst } from './custom-resizer-first';
 
 export const App = () => {
+  const pane1 = 'pane1'
   const pane2 = 'pane2'
   const pane3 = 'pane3'
 
   return (
     <div>
       <div>
-        <h3 className='t-aligin-center'>Horizontal Panes</h3>
+        <h3 className='t-color-mainBlue t-aligin-center'>Nested Panes</h3>
       </div>
+
       <ResizablePanes
         resizer={
-          <CustomResizerFirst horizontal />
+          <CustomResizerFirst />
             }
+        vertical
       >
-
         <Panes
-          className={pane2} id={pane2} size={150}
+          className={pane1} id={pane1} minSize={100}
+
+          size={200}
         >
+          <ResizablePanes
+            resizer={
+              <CustomResizerFirst horizontal />
+            }
+          >
+            <Panes
+              className={pane1} id={pane1} size={150}
+            >
+            </Panes>
+            <Panes className={pane3} id={pane3} size={150}> </Panes>
+          </ResizablePanes>
         </Panes>
 
         <Panes
-          className={pane3} id={pane3} size={150}
+          className={pane2} id={pane2} minSize={100} size={400}
         >
         </Panes>
 
+        <Panes className={pane3} id={pane3} minSize={100} size={200}>
+          <ResizablePanes
+            resizer={
+              <CustomResizerFirst horizontal />
+            }
+          >
+            <Panes
+              className={pane1} id={pane1} size={150}
+            >
+            </Panes>
+            <Panes className={pane3} id={pane3} size={150}> </Panes>
+          </ResizablePanes>
+        </Panes>
       </ResizablePanes>
 
     </div>
-
   )
 }
+
 
 export default App;
