c9c661dde5c82524a346dcc2fac1a23834dcddd5
BipanKishore <bipankishore@gmail.com>
2023-09-27 19:41:12 +0530

    show-and-hide-demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,92 @@
-import React from 'react';
+import React, { Ref, useRef, useState } from 'react';
 import './App.css';
 import {
+  IResizableApi,
   Panes, ResizablePanes
 } from 'resizable-panes-react'
-import { CustomResizerFirst } from './custom-resizer-first';
 
 
-export const App = () => {
-  const pane1 = 'pane1'
-  const pane2 = 'pane2'
-  const pane3 = 'pane3'
+  interface IIDMap{
+    [id: string]: boolean
+}
+
+ const App = () => {
+  const ref: Ref<any> = useRef({})
+
+  const [visibilityMap, setVisibilityMap] = useState<IIDMap>({
+    pane1: true,
+    pane2: true,
+    pane3: true,
+    pane4: true
+  })
+
+  const onReady = (api: IResizableApi) => {
+    ref.current.api = api
+  }
+  const updateVisibilityMap = (e: any) => {
+    const {name, checked} = e.currentTarget
+    const newVisibilityMap = {
+      ...visibilityMap,
+      [name]: checked
+    }
+    setVisibilityMap(newVisibilityMap)
+  }
+
+  const setVisibility = () => {
+    ref.current.api.setVisibility(visibilityMap)
+  }
+
   return (
     <div>
       <div>
-        <h3 className='t-color-mainBlue t-aligin-center'>Custom Resizer for Panes</h3>
+        <h3 className='t-color-mainBlue t-aligin-center'>Show and hide panes</h3>
+        <h6>Note: Experimental features!</h6>
       </div>
 
-      <div className='m-20-0' >
-        <ResizablePanes
-          className='h-300' resizer={
-            <CustomResizerFirst />
-            }
-          vertical
-        >
-          <Panes
-            className={pane1} id={pane1}
-            size={350}
-          >
-          </Panes>
+      <ResizablePanes
+        className='h-300'
+        vertical
+        onChangeVisibility={(e:any) => {
+          console.log('onChangeVisibility', e)
+        }}
+        onReady={onReady}
+      >
+        <Panes className={'pane1'} id={'pane1'} minSize={50} size={200}>
+        </Panes>
 
-          <Panes
-            className={pane2} id={pane2}
-            size={300}
-          >
-          </Panes>
+        <Panes className={'pane2'} id={'pane2'} maxSize={150} minSize={50} size={100}>
+        </Panes>
 
-          <Panes className={pane3} id={pane3} size={150}>
-          </Panes>
+        <Panes className={'pane3'} id={'pane3'} maxSize={200} minSize={100} size={200}>
+        </Panes>
 
-        </ResizablePanes>
+        <Panes className={'pane1'} id={'pane4'} maxSize={250} minSize={50} size={200}>
+        </Panes>
+      </ResizablePanes>
+
+      <div className='m-20-0'>
+        {Object
+          .keys(visibilityMap)
+          .map((id) => (
+
+            <label key={id}>
+              <input
+                checked={visibilityMap[id]}
+                name={id}
+                type="checkbox"
+                onChange={updateVisibilityMap}
+              />
+              {id}
+            </label>
+
+          ))}
+
+        <button onClick={setVisibility} >Submit</button>
       </div>
     </div>
   )
 }
+
 
 
 export default App;
