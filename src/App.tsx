1b9a647118d1aa891543db6508e718c5431a4756
BipanKishore <142712498+BipanKishore@users.noreply.github.com>
2023-09-27 19:33:55 +0530

    2 files modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,27 @@
 import React, { useRef } from 'react';
 import './App.css';
-import {
-  Panes, ResizablePanes
-} from 'resizable-panes-react'
+import { Panes, ResizablePanes } from 'resizable-panes-react';
 import { CustomResizerFirst } from './custom-resizer-first';
 import PaneModesIcons from './pane-modes-icons';
 
-export const App  = () => {
-  const resizableRef = useRef<any>({})
-  const pane1 = 'pane1'
-  const pane2 = 'pane2'
+export const App = () => {
+  const resizableRef = useRef<any>({});
+  const pane1 = 'pane1';
+  const pane2 = 'pane2';
 
   return (
     <div>
       <div>
-        <h3 className='t-color-mainBlue t-aligin-center'>Fullscreen modes</h3>
+        <h3 className="t-color-mainBlue t-aligin-center">Fullscreen modes</h3>
       </div>
 
       <ResizablePanes
-        resizer={
-          <CustomResizerFirst horizontal />
-          }
+        resizer={<CustomResizerFirst horizontal />}
         onReady={(api: any) => {
-          resizableRef.current.api = api
+          resizableRef.current.api = api;
         }}
       >
-        <Panes
-          className={pane1} id={pane1} size={200}
-        >
+        <Panes className={pane1} id={pane1} size={200}>
           <PaneModesIcons id={pane1} resizableRef={resizableRef} />
         </Panes>
 
@@ -35,9 +29,8 @@ export const App  = () => {
           <PaneModesIcons id={pane2} resizableRef={resizableRef} />
         </Panes>
       </ResizablePanes>
-
     </div>
-  )
-}
+  );
+};
 
 export default App;
