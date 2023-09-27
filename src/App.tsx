1e867faa1d3d02a0a2ff9c0d4823fbca2cd3e714
BipanKishore <bipankishore@gmail.com>
2023-09-27 18:59:42 +0530

    simple-pane-demo

--- src/App.tsx
+++ src/App.tsx
@@ -3,38 +3,36 @@ import './App.css';
 import {
   Panes, ResizablePanes
 } from 'resizable-panes-react'
-import { CustomResizerFirst } from './custom-resizer-first';
 
 export const App = () => {
+  const pane1 = 'pane1'
   const pane2 = 'pane2'
   const pane3 = 'pane3'
-
   return (
     <div>
       <div>
-        <h3 className='t-aligin-center'>Horizontal Panes</h3>
+        <h3 className='t-color-mainBlue t-aligin-center'>Simple Panes</h3>
       </div>
-      <ResizablePanes
-        resizer={
-          <CustomResizerFirst horizontal />
-            }
-      >
 
-        <Panes
-          className={pane2} id={pane2} size={150}
-        >
-        </Panes>
+      <div className='m-20-0' >
+        <ResizablePanes>
+          <Panes
+            className={pane1} id={pane1} size={100}
+          >
+          </Panes>
 
-        <Panes
-          className={pane3} id={pane3} size={150}
-        >
-        </Panes>
+          <Panes
+            className={pane2} id={pane2} size={150}
+          >
+          </Panes>
 
-      </ResizablePanes>
-
+          <Panes className={pane3} id={pane3} size={120}>
+          </Panes>
+        </ResizablePanes>
+      </div>
     </div>
-
   )
 }
+
 
 export default App;
