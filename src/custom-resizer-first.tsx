54cd57659ea22de202803aced10dc2fced8022a4
BipanKishore <142712498+BipanKishore@users.noreply.github.com>
2023-09-28 01:38:46 +0530

    1 file modified

--- src/custom-resizer-first.tsx
+++ src/custom-resizer-first.tsx
@@ -1,27 +1,26 @@
-import React from 'react'
-import {joinClassName} from './util'
+import React from 'react';
+import { joinClassName } from './util';
 
-export const CustomResizerFirst = ({horizontal}: {
-  horizontal: boolean
-}) => {
+export const CustomResizerFirst = ({ horizontal }: { horizontal: boolean }) => {
   const parentClassName = joinClassName({
     'custom-resizer-1st-parent': true,
     'h-100p w-12': !horizontal,
-    'h-12': horizontal
-  })
+    'h-12': horizontal,
+  });
 
   const childClassName = joinClassName({
     'custom-resizer-1st-child': true,
-    'h-100p w-100p': true
-  })
+    'h-100p w-100p': true,
+  });
 
   return (
     <div className={parentClassName}>
       <div className={childClassName}></div>
     </div>
-  )
-}
+  );
+};
 
 CustomResizerFirst.defaultProps = {
-    horizontal: false
-}
+  horizontal: false,
+};
+console.log('test');
