3335f94b6592781e480d6fb0bfc656fe92f75833
BipanKishore <142712498+BipanKishore@users.noreply.github.com>
2023-09-27 19:15:19 +0530

    2 files modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,41 @@
 import React from 'react';
 import './App.css';
-import {
-  Panes, ResizablePanes
-} from 'resizable-panes-react'
+import { Panes, ResizablePanes } from 'resizable-panes-react';
 import { CustomResizerFirst } from './custom-resizer-first';
 import { CustomResizerSecond } from './custom-resizer-second';
 
 export const App = () => {
-  const pane1 = 'pane1'
-  const pane2 = 'pane2'
-  const pane3 = 'pane3'
+  const pane1 = 'pane1';
+  const pane2 = 'pane2';
+  const pane3 = 'pane3';
   return (
     <div>
       <div>
-        <h3 className='t-color-mainBlue t-aligin-center'>Custom Resizer for Panes</h3>
+        <h3 className="t-color-mainBlue t-aligin-center">
+          Custom Resizer for Panes
+        </h3>
       </div>
 
-      <div className='m-20-0' >
+      <div className="m-20-0">
         <ResizablePanes
-          className='h-300' resizer={
-            <CustomResizerFirst />
-            }
+          className="h-300"
+          resizer={<CustomResizerFirst />}
           vertical
         >
-          <Panes
-            className={pane1} id={pane1}
-            size={350}
-          >
-          </Panes>
+          <Panes className={pane1} id={pane1} size={350}></Panes>
 
           <Panes
-            className={pane2} id={pane2}
-            resizer={
-              <CustomResizerSecond />
-            }
+            className={pane2}
+            id={pane2}
+            resizer={<CustomResizerSecond />}
             size={300}
-          >
-          </Panes>
+          ></Panes>
 
-          <Panes className={pane3} id={pane3} size={150}>
-          </Panes>
-
+          <Panes className={pane3} id={pane3} size={150}></Panes>
         </ResizablePanes>
       </div>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
