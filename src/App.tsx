931b5d3c1752c3600740a67ee64e76745cc6e502
BipanKishore <bipankishore@gmail.com>
2023-09-27 19:13:17 +0530

    custom-resizer-for-panes

--- src/App.tsx
+++ src/App.tsx
@@ -4,37 +4,48 @@ import {
   Panes, ResizablePanes
 } from 'resizable-panes-react'
 import { CustomResizerFirst } from './custom-resizer-first';
+import { CustomResizerSecond } from './custom-resizer-second';
 
 export const App = () => {
+  const pane1 = 'pane1'
   const pane2 = 'pane2'
   const pane3 = 'pane3'
-
   return (
     <div>
       <div>
-        <h3 className='t-aligin-center'>Horizontal Panes</h3>
+        <h3 className='t-color-mainBlue t-aligin-center'>Custom Resizer for Panes</h3>
       </div>
-      <ResizablePanes
-        resizer={
-          <CustomResizerFirst horizontal />
+
+      <div className='m-20-0' >
+        <ResizablePanes
+          className='h-300' resizer={
+            <CustomResizerFirst />
             }
-      >
-
-        <Panes
-          className={pane2} id={pane2} size={150}
+          vertical
         >
-        </Panes>
+          <Panes
+            className={pane1} id={pane1}
+            size={350}
+          >
+          </Panes>
 
-        <Panes
-          className={pane3} id={pane3} size={150}
-        >
-        </Panes>
+          <Panes
+            className={pane2} id={pane2}
+            resizer={
+              <CustomResizerSecond />
+            }
+            size={300}
+          >
+          </Panes>
 
-      </ResizablePanes>
+          <Panes className={pane3} id={pane3} size={150}>
+          </Panes>
 
+        </ResizablePanes>
+      </div>
     </div>
-
   )
 }
+
 
 export default App;
---
+++ src/custom-resizer-second.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {joinClassName} from './util'
+
+export const CustomResizerSecond = ({horizontal}: {
+  horizontal: boolean
+}) => {
+  const parentClassName = joinClassName({
+    'custom-resizer-1st-parent': true,
+    'h-100p w-12': !horizontal,
+    'h-12': horizontal
+  })
+
+  const childClassName = joinClassName({
+    'custom-resizer-1st-child': true,
+    'h-100p w-100p': true
+  })
+
+  return (
+    <div className={parentClassName}>
+      <div className={childClassName}></div>
+    </div>
+  )
+}
+
+CustomResizerSecond.defaultProps = {
+    horizontal: false
+}
