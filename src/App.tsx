86bb7c1b9e84188276ef1f74c0efb45f1852148b
BipanKishore <142712498+BipanKishore@users.noreply.github.com>
2023-09-27 20:00:24 +0530

    2 files modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,24 @@
 import React from 'react';
 import './App.css';
-import {
-  Panes, ResizablePanes
-} from 'resizable-panes-react'
+import { Panes, ResizablePanes } from 'resizable-panes-react';
 import { CustomResizerFirst } from './custom-resizer-first';
 
 export const App = () => {
-  const pane2 = 'pane2'
-  const pane3 = 'pane3'
+  const pane2 = 'pane2';
+  const pane3 = 'pane3';
 
   return (
     <div>
       <div>
-        <h3 className='t-aligin-center'>Horizontal Panes</h3>
+        <h3 className="t-aligin-center">Horizontal Panes</h3>
       </div>
-      <ResizablePanes
-        resizer={
-          <CustomResizerFirst horizontal />
-            }
-      >
+      <ResizablePanes resizer={<CustomResizerFirst horizontal />}>
+        <Panes className={pane2} id={pane2} size={150}></Panes>
 
-        <Panes
-          className={pane2} id={pane2} size={150}
-        >
-        </Panes>
-
-        <Panes
-          className={pane3} id={pane3} size={150}
-        >
-        </Panes>
-
+        <Panes className={pane3} id={pane3} size={150}></Panes>
       </ResizablePanes>
-
     </div>
-
-  )
-}
+  );
+};
 
 export default App;
